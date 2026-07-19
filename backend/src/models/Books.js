f371b78b37b71572dc86d05e5d452835980076ce
Oluwaseyifunmi Banjo <banjoseyi2@gmail.com>
2026-07-19 17:43:28 +0100

    mongooes Schema added

--- backend/src/models/Books.js
+++ backend/src/models/Books.js
@@ -1,2 +1,20 @@
 const mongoose = require("mongoose");
 
+// const reviewSchema = new mongoose.Schema({
+//     name: String,
+//     body: String
+// });
+
+const bookSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    pages: Number,
+    rating: Number,
+    genres: [String],
+    reviews: [{
+        name: String,
+        body: String
+    }]
+});
+
+module.exports = mongoose.model("Book", bookSchema)
